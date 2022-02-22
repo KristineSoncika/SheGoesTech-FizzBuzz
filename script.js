9ef8ed25cd6ecc99f04ad482f1f02b624ceebae4
kristine <57680335+kristtiine@users.noreply.github.com>
2022-02-22 21:16:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let enteredNum = parseInt(
+  prompt("How many FizzBuzz numbers do you wanto to see?")
+);
+let fizzBuzzNums = [];
+for (let i = 15; i <= 15 * enteredNum; i += 15) {
+  fizzBuzzNums.push(i);
+  console.log(fizzBuzzNums);
+}
+
+document.getElementById(
+  "fizzBuzz"
+).innerHTML = `These are the first ${enteredNum} FizzBuzz numbers: ${fizzBuzzNums.join(
+  ", "
+)}`;
